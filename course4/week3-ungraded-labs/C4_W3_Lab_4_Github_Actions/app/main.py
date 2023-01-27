6486ed93d989b299a6cbf703cc6c33ed039596e9
calvar <carl.alv@gmail.com>
2023-01-27 07:08:06 -0500

    Added comment to main.py to test CICD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+#Added comment to test actions
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
